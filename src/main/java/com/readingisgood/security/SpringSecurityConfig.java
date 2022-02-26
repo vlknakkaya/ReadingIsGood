c58e89ec6d4f75d822b97ff996fd5138994c7b43
Volkan <vlknakkaya@gmail.com>
2022-02-26 13:09:47 +0300

    added swagger permits

--- src/main/java/com/readingisgood/security/SpringSecurityConfig.java
+++ src/main/java/com/readingisgood/security/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +17,15 @@ import com.readingisgood.service.DummyUserDetailsService;
 
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
+	private static final String[] SWAGGER_PERMITS = {
+			"/v3/api-docs/**",
+			"/swagger-ui/**",
+			"/api-doc.html",
+			"/swagger-resources/**",
+			"/webjars/**",
+	};
+	
 	@Autowired
 	private DummyUserDetailsService userDetailsService;
 	@Autowired
@@ -30,13 +39,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	    http.cors().and().csrf().disable()
-	            .authorizeRequests().antMatchers("/auth").permitAll()
+	            .authorizeRequests()
+	            .antMatchers("/auth").permitAll()
+	            .antMatchers(SWAGGER_PERMITS).permitAll()
 	            .anyRequest().authenticated()
 	            .and()
 	            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	    
 	    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 	}
+	
+	@Override
+    public void configure(WebSecurity web)  {
+        web.ignoring().antMatchers(SWAGGER_PERMITS);
+    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
